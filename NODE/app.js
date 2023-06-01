65317c4898c399b03edb39c09d33ee1926d180f6
Swapnil <90739364+SwapnilTekale@users.noreply.github.com>
2023-06-01 07:37:57 +0530

    Add files via upload

---
+++ NODE/app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile("./public/index.html");
+});
+
+app.listen(PORT, () => console.log(`Server listening on port :${PORT}`));
